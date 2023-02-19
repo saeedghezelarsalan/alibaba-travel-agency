afbdc06e4404fdec070afacb4b3428c2527dfb5b
Saeed Ghezel Arsalan <saeedghezelarsalan@gmail.com>
2023-02-19 16:02:34 +0330

    add ssr and MobileNav

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,9 @@
+import axios from "axios";
 import Head from "next/head";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import MobileNav from "../components/MobileNav";
 
-const Home = () => {
+const Home = ({ accordion, homePageDescription, iranState }) => {
   return (
     <div className="flex flex-col lg:mt-16 bg-[#F6F6F6] dark:bg-[#222222] h-auto ">
       <Head>
@@ -11,10 +14,41 @@ const Home = () => {
         />
       </Head>
       <div>
-        
+
+        <MobileNav />
+
       </div>
     </div>
   );
 };
 
 export default Home;
+
+
+export const getStaticProps = async ({ locale }) => {
+
+  let accordion = await axios.get(
+    "http://localhost:3001/domesticFlightAccordion"
+  );
+  accordion = accordion.data;
+  let homePageDescription = await axios.get(
+    "http://localhost:3001/domesticFlightDescription"
+  );
+  homePageDescription = homePageDescription.data;
+  let iranState = await axios.get("http://localhost:3001/iranState");
+  iranState = iranState.data;
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, [
+        "common",
+        "navbar",
+        "travelSupport",
+        "application",
+        "footer",
+      ])),
+      accordion,
+      homePageDescription,
+      iranState,
+    },
+  };
+};
