8284e1d1c4851723a38321ac0737a916e9a1262e
Saeed Ghezel Arsalan <saeedghezelarsalan@gmail.com>
2023-02-19 21:14:50 +0330

    add DarkMode to project

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,6 +4,8 @@ import Navbar from "../provider/Navbar";
 import { appWithTranslation } from "next-i18next";
 import BorderProvider from "../Provider/BorderProvider";
 import Footer from "../components/Footer";
+import DarkMode from "../components/DarkMode";
+import { ThemeProvider } from "next-themes";
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -19,12 +21,15 @@ function MyApp({ Component, pageProps }) {
           href="https://cdn.alibaba.ir/h2/desktop/assets/images/logo-alibaba-square-logo-small-1fb61159.svg"
         />
       </Head>
+      <ThemeProvider attribute="class">
       <Navbar>
         <BorderProvider>
           <Component {...pageProps} />
         </BorderProvider>
       </Navbar>
       <Footer />
+      <DarkMode />
+      </ThemeProvider>
     </div>
   );
 }
