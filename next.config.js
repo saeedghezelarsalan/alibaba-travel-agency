793781df1dc7adbab04141f70b2a99986f247539
Saeed Ghezel Arsalan <saeedghezelarsalan@gmail.com>
2023-02-24 23:39:15 +0330

    fix error

--- next.config.js
+++ next.config.js
@@ -3,14 +3,6 @@ const { i18n } = require("./next-i18next.config");
 
 
 module.exports = {
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ["@svgr/webpack"]
-    });
-  
-    return config;
-  },
   images: {
     domains: ["cdn.alibaba.ir"],
   },
